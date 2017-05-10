47d7269a9220ce304f0dc11a2851d36f364dfbca
Himanshu Rohilla <ruhela.himanshu@gmail.com>
2017-05-10 10:24:27 +0530

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ global.__base = __dirname + '/';
 global.path_to_app_constants = __dirname + '/app/utility/constants';
 var app = require('./app');
 
-var server_port = 8080
+var server_port = process.env.PORT || 3000;
 // var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.listen(server_port, function(){
