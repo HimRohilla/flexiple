5f25871e9eb979da70c471124813f5088af49efc
Himanshu Rohilla <ruhela.himanshu@gmail.com>
2017-05-10 10:34:22 +0530

    changed

--- app/utility/constants.js
+++ app/utility/constants.js
@@ -8,12 +8,12 @@ function define(name, value) {
 		configurable: false
 	});
 }
-define("host", "localhost");
-define("username", "root");
-define("password", "");
-define("db_name", "flexiple_db");
-define("URL", "http://localhost:3000/");
-define("domain", "localhost:3000");
+define("host", "sql11.freemysqlhosting.net");
+define("username", "sql11173574");
+define("password", "gU8gBz7X9V");
+define("db_name", "sql11173574");
+define("URL", "https://desolate-ravine-98491.herokuapp.com/");
+define("domain", "desolate-ravine-98491.herokuapp.com");
 define("session_secret", 'ANY_SECRET');
 define("path_to_app_controllers", __base + "app/controllers/");
 define("path_to_app_models", __base + "app/models/");
