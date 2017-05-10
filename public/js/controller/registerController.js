c9c94d5e237e5ec0adc8cc8d1d5b8b4b359eda7b
Himanshu Rohilla <ruhela.himanshu@gmail.com>
2017-05-10 10:54:36 +0530

    final changes

--- public/js/controller/registerController.js
+++ public/js/controller/registerController.js
@@ -2,6 +2,7 @@ var mainApp = angular.module('mainApp');
 
 mainApp.controller('RegisterController', ['$scope', '$http', function($scope, $http) {
 	$scope.passwordIsInCorrect = true;
+	$detailsIncorrect = false;
 	$scope.detailsCorrectMessage = "";
 	$scope.checkPassword = function(formData) {
 		if(formData.password !== formData.confirm_password) {
@@ -10,32 +11,42 @@ mainApp.controller('RegisterController', ['$scope', '$http', function($scope, $h
 		else {
 			$scope.passwordIsInCorrect = false;
 		}
+		$scope.checkDetails();
 	}
-	$scope.checkDetails = function(formData) {
+	$scope.checkDetails = function() {
+		console.log($scope.formData);
+		var formData = $scope.formData;
 		var format = /[!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?A-Z0-9]+/;
 		var emailFormat = /[@]/;
-		if(format.test(formData.name)) {
+		if(formData.name !== undefined && format.test(formData.name)) {
 			$scope.detailsCorrectMessage = "Name is not in valid format";
+			$scope.detailsIncorrect = true;
 		}
-		else if(formData.name.substr(0, 9) != "flexiple_") {
+		else if(formData.name !== undefined && formData.name.substr(0, 9) != "flexiple_") {
 			$scope.detailsCorrectMessage = "Name must start with 'flexiple_'";
+			$scope.detailsIncorrect = true;
 		}
-		else if(!emailFormat.test(formData.username)) {
+		else if(formData.username !== undefined && !emailFormat.test(formData.username)) {
 			$scope.detailsCorrectMessage = "email must contain @";
+			$scope.detailsIncorrect = true;
 		}
-		else if(formData.username.substr(formData.username.length - 3, 3) != ".in") {
+		else if(formData.username !== undefined && formData.username.substr(formData.username.length - 3, 3) != ".in") {
 			$scope.detailsCorrectMessage = "only .in domain emails are allowed";
+			$scope.detailsIncorrect = true;
 		}
-		else if(formData.password.replace(/[^A-Z]/g, "").length != 2 || formData.password.replace(/[^!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?]/g, "").length != 2) {
+		else if(formData.password !== undefined && (formData.password.replace(/[^A-Z]/g, "").length != 2 || formData.password.replace(/[^!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?]/g, "").length != 2)) {
 			$scope.detailsCorrectMessage = "Password must contains 2 uppercase letter and 2 special characters";
+			$scope.detailsIncorrect = true;
 		}
 		else {
+			$scope.detailsCorrectMessage = "";
+			$scope.detailsIncorrect = false;
 			return true;
 		}
 		return false;
 	};
 	$scope.registerUser = function(formData) {
-		if($scope.checkDetails(formData)) {
+		if($scope.checkDetails()) {
 			$http({
 				method  : 'POST',
 				url     : '/user/registerHandler',
