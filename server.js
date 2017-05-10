8ddad22b80d2615a3c95d68b535efee0ad08ba88
HimRohilla <ruhela.himanshu@gmail.com>
2017-05-10 07:47:36 +0530

    update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ global.__base = __dirname + '/';
 global.path_to_app_constants = __dirname + '/app/utility/constants';
 var app = require('./app');
 
-app.listen(3000, function(){
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function(){
 	console.log('listening on *:3000');
 });
